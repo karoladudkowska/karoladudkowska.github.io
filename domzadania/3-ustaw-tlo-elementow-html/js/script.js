c631098fa2c1dff1e4844de7ae65b5fa395c7fd4
Karolina Dudkowska <karoladudkowska@wp.pl>
2020-12-30 16:30:26 +0100

    set background-color for two Elements

--- domzadania/3-ustaw-tlo-elementow-html/js/script.js
+++ domzadania/3-ustaw-tlo-elementow-html/js/script.js
@@ -1,12 +1,48 @@
-let first = document.getElementById('firstP');
-let second = document.getElementById('secondP');
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
 
-console.log(document);
-const changeColor = () => {
-
-    first.style.background = 'red';
+    p1.classList.add('bgRed');
+    p2.classList.add('bgYellow')
 }
 
-let btn = document.getElementById('change');
-// btn.onclick = changeColor;
-btn.addEventListener('click', changeColor);
+let btn = document.getElementById('setBackground');
+
+btn.addEventListener('click', setBackground);
+
+// lub 2 sposób
+
+// const setBackground = () => {
+//     let p1 = document.querySelector('.first');
+//     let p2 = document.querySelector('.last');
+
+//     p1.style.background = 'red';
+//     p2.style.background = 'yellow';
+// }
+
+// let btn = document.getElementById('setBackground');
+
+// btn.addEventListener('click', setBackground);
+
+
+
+
+
+
+// lub 3 sposób
+
+
+// let first = document.getElementById('firstP');
+// let second = document.getElementById('secondP');
+
+// console.log(document);
+// const setBackground = () => {
+
+//     first.style.background = 'red';
+//     second.style.background = 'yellow';
+// }
+
+// let btn = document.getElementById('setBackground');
+
+// btn.addEventListener('click', setBackground);
+
