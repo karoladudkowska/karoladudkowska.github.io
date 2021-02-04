3540c1884f699bc059542a9032ed71f395ca1ab0
Karolina Dudkowska <karoladudkowska@wp.pl>
2021-02-04 20:33:53 +0100

    section services i procedures

--- html-css-js-sass/js/main.js
+++ html-css-js-sass/js/main.js
@@ -1,9 +1,9 @@
 document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click', function () {
     document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
-}
-);
+    console.log("otwieram");
+});
 
 document.getElementsByClassName('mobile-close')[0].addEventListener('click', function () {
     document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
-}
-);
+    console.log("zamykam");
+});
